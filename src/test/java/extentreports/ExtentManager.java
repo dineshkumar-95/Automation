706138017b6794dffc6ab3a35c128ad811611b68
dineshkumar-95 <dineshkec95@gmail.com>
2026-08-20 12:59:13 +0530

    Update ExtentManager.java

--- src/test/java/extentreports/ExtentManager.java
+++ src/test/java/extentreports/ExtentManager.java
@@ -10,7 +10,7 @@ public class ExtentManager {
 
     public static synchronized ExtentReports getReporter() {
         if (extent == null) {
-            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
+            String reportPath = System.getProperty("user.dir") + "/ExtentReport/ExtentReport.html";
             ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
 //            sparkReporter.config().setDocumentTitle("Automation Report");
 //            sparkReporter.config().setReportName("Test Execution Report");
